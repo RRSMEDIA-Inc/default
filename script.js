2d630d1085692624f18b5fbde148646801ba915a
Rashmi Ranjan <57025766+imrashmi@users.noreply.github.com>
2023-08-30 21:52:58 +0530

    script_js

---
+++ script.js
@@ -0,0 +1,19 @@
+const generatePassword = () => {
+    const length = document.getElementById('length').value;
+    const lowercase = 'abcdefghijklmnopqrstuvwxyz';
+    const uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const numbers = '0123456789';
+    const symbols = '!@#$%^&*()_-+=<>?';
+
+    const allChars = lowercase + uppercase + numbers + symbols;
+
+    let password = '';
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * allChars.length);
+        password += allChars[randomIndex];
+    }
+
+    document.getElementById('result').textContent = password;
+};
+
+document.getElementById('generate').addEventListener('click', generatePassword);
