fb690c3bebb2f898f23d07744bb552b97ae6a28f
Rashmi Ranjan <57025766+imrashmi@users.noreply.github.com>
2023-08-30 22:43:14 +0530

    script

--- script.js
+++ script.js
@@ -1,34 +1,7 @@
-const generatePassword = () => {
-    const length = document.getElementById('length').value;
-    const lowercase = 'abcdefghijklmnopqrstuvwxyz';
-    const uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    const numbers = '0123456789';
-    const symbols = '!@#$%^&*()_-+=<>?';
+document.getElementById('password-button').addEventListener('click', function() {
+    window.location.href = 'https://inc.rrsmedia.co.in/pass_gen';
+});
 
-    const allChars = lowercase + uppercase + numbers + symbols;
-
-    let password = '';
-    for (let i = 0; i < length; i++) {
-        const randomIndex = Math.floor(Math.random() * allChars.length);
-        password += allChars[randomIndex];
-    }
-
-    document.getElementById('result').textContent = password;
-    document.getElementById('copy').disabled = false;
-};
-
-const copyToClipboard = () => {
-    const password = document.getElementById('result').textContent;
-
-    const textarea = document.createElement('textarea');
-    textarea.value = password;
-    document.body.appendChild(textarea);
-    textarea.select();
-    document.execCommand('copy');
-    document.body.removeChild(textarea);
-
-    alert('Password copied to clipboard!');
-};
-
-document.getElementById('generate').addEventListener('click', generatePassword);
-document.getElementById('copy').addEventListener('click', copyToClipboard);
+document.getElementById('passphrase-button').addEventListener('click', function() {
+    window.location.href = 'https://inc.rrsmedia.co.in/passphrase';
+});
